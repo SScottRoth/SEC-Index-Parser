11267983c5bfbb967d1d6f5adb3c86df3477808c
SScottRoth <58118784+SScottRoth@users.noreply.github.com>
2020-01-24 15:44:26 -0500

    Update Index Parser for Listed Filings Rev3.py
    pull all files into an array first - put some print statements to show what's going on.

--- Index Parser for Listed Filings Rev3.py
+++ Index Parser for Listed Filings Rev3.py
@@ -9,7 +9,7 @@ data_folder = Path('C:/Users/sscot/Dropbox (SRCMLLC)/SRCM/Python/Output/Edgar')
 data_folder_out = Path('C:/Users/sscot/Dropbox (SRCMLLC)/SRCM/Python/Output/Edgar Out')
 
 
-write_file = "FinFilings.txt"
+write_file = "FinFilingsProsp.txt"
 file_to_write = data_folder_out / write_file
 
 
@@ -18,8 +18,9 @@ myFile = open(file_to_write, 'w')
 linesIn = 0
 linesOut= 0
 
-
+print("reading files")
 for y in range(1993, 2020):
+    print('adding '+ str(y))
     for q in range(1,5):
         open_file = str(y) + '-QTR' + str(q) + '.tsv'
         file_to_open = data_folder / open_file
@@ -28,17 +29,18 @@ for y in range(1993, 2020):
             for row in csv.reader(csv_file, delimiter='|'):
                 inputs.append(row)
                 linesIn = linesIn + 1
-        req_forms = ['10-K','10-Q','10-K/A','10-Q/A','10-QT', '10-KT','20-F','20-F/A']
-        output = [x for x in inputs if x[2] in req_forms]
+req_forms = ['F-1','S-1','S-1/A','F-1/A','424A','424B1','424B2','424B3','424B4','424B5','424B6','424B7','424B8']
+print("filtering file")
+output = [x for x in inputs if x[2] in req_forms]
+print("writing file")
+myFile = open(file_to_write, 'a', newline='')
+with myFile:
+    writer = csv.writer(myFile)
+    writer.writerows(output)
+    print(open_file)
 
-        myFile = open(file_to_write, 'a', newline='')
-        with myFile:
-            writer = csv.writer(myFile)
-            writer.writerows(output)
-            print(open_file)
-
-       
-index_write_file = "CIK_Name_Index.txt"
+print("writing index")      
+index_write_file = "CIK_Name_IndexProsp.txt"
 CIK_Name_to_write = data_folder_out / index_write_file
 CIK_Name_Unique = {}
 
@@ -51,7 +53,7 @@ with open(file_to_write) as csv_file:
             linesOut = linesOut + 1
 
 print("Lines in " + str(linesIn))
-print("Lines in " + str(linesOut))
+print("Lines out " + str(linesOut))
 
 sorted_x = sorted(CIK_Name_Unique.items(), key=operator.itemgetter(1))
 sorted_dict = collections.OrderedDict(sorted_x)
