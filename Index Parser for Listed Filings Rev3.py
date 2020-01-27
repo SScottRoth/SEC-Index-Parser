e8cfa9c5c8af967888cbc10f802a88e7f813a550
SScottRoth <58118784+SScottRoth@users.noreply.github.com>
2020-01-27 11:20:17 -0500

    Added new function to append timedate , cleaned up code

--- Index Parser for Listed Filings Rev3.py
+++ Index Parser for Listed Filings Rev3.py
@@ -6,36 +6,47 @@ import collections
 import pprint
 import time
 
-data_folder = Path('C:/Users/sscot/Dropbox (SRCMLLC)/SRCM/Python/Output/Edgar')
+# updated to have input files be in a new input directory
+# added date and time one end of new files written
+# need to add flag to write_records to include header or not
+# for time being, just wrote new function
+
+data_folder = Path('C:/Users/sscot/Dropbox (SRCMLLC)/SRCM/Python/Input/Edgar Index Files')
 data_folder_out = Path('C:/Users/sscot/Dropbox (SRCMLLC)/SRCM/Python/Output/Edgar Out')
 
+def file_to_write(file_name):
+    timestr = time.strftime("%Y-%m-%d-%H-%M")
+    full_file_name = file_name + " " + timestr + ".txt"
+    file_and_path = data_folder_out / full_file_name
+    return(str(file_and_path))
 
-write_file = "FinFilings.txt"
-file_to_write = data_folder_out / write_file
-
-def write_records(file_to_write, records):
-    with open(file_to_write, 'w', newline='') as myFile:
+def write_records(input_file, records):
+    with open(input_file, 'w', newline='') as myFile:
         writer = csv.writer(myFile)
         writer.writerow(['cik', 'name', 'form_type', 'filing_date', 'form_text_url', 'form_html_url'])
         writer.writerows(records)
-       
+
+def write_records_no_header(input_file, records):
+    with open(input_file, 'w', newline='') as myFile:
+        writer = csv.writer(myFile)
+        writer.writerows(records)
+
+ # Load in all quarterly index files into a list named datarecords     
 
 datarecords = []
-start = time.clock()
-for y in range(1993, 2020):
+#start = time.clock()
+for y in range(1993, 1995):
     for q in range(1,5):
         open_file = str(y) + '-QTR' + str(q) + '.tsv'
         file_to_open = data_folder / open_file
         with open(file_to_open, 'r') as csv_file:
-            print("{}...{}".format(open_file, time.clock() - start))
-            #reader = csv.reader(csv_file, delimiter='|')
-            #inputs.extend(list(reader))
+#            print("{}...{}".format(open_file, time.clock() - start))
             datarecords.extend([_.strip().split('|') for _ in csv_file.readlines()])
-            #for row in csv.reader(csv_file, delimiter='|'):
-            #    inputs.append(row)
-            #    linesIn = linesIn + 1
 
-FORM_TYPE_FLD = 2
+write_records(file_to_write("All_SEC_Filings_Index"), datarecords)
+
+       
+FORM_TYPE_FLD = 2   # 3rd element contains the form type
 
 # create dictionary where defult/new keys will be integers of 0
 form_type_counts = collections.defaultdict(int)
@@ -44,55 +55,16 @@ for row in datarecords:
     form_type_counts[row[FORM_TYPE_FLD]] += 1
     form_cik_counts[row[FORM_TYPE_FLD]].add(row[0])
 
-print("Form Type/CIK Counts")
-with open(data_folder_out / "Form Type and CIK Counts.txt", 'w') as f:
+with open(file_to_write("Form_Type_CIK_Count"), 'w') as f:
     for key in sorted(form_type_counts.keys()):
         f.write("{:20}  {:5d}  {:5d}\n".format(key, form_type_counts[key], len(form_cik_counts[key])))
 
 req_forms = ['10-K','10-Q','10-K/A','10-Q/A','10-QT', '10-KT','20-F','20-F/A', '6-F']
-output = [row for row in datarecords if row[FORM_TYPE_FLD] in req_forms]
-
-write_records(data_folder_out / "AllFinFilings.txt", datarecords)
-write_records(file_to_write, output)
+filtered_Filings = [row for row in datarecords if row[FORM_TYPE_FLD] in req_forms]
+write_records(file_to_write("Filtered_Qtr_Filers"), filtered_Filings)
 
 
-index_write_file = "CIK_Name_Index.txt"
-CIK_Name_to_write = data_folder_out / index_write_file
-CIK_Name_Unique = {}
-
-with open(file_to_write) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    for row in csv_reader:
-        key = row[0] + "-" + row[1]
-        if key not in CIK_Name_Unique:
-            CIK_Name_Unique.update({key : row})
-
-sorted_x = sorted(CIK_Name_Unique.items(), key=operator.itemgetter(1))
-sorted_dict = collections.OrderedDict(sorted_x)
-
-(pd.DataFrame.from_dict(data=sorted_dict, orient='index')
-   .to_csv(CIK_Name_to_write, header=False))
-
-
-#      print(str(y)+"-QTR"+str(q))
-#      print("Lines in " + str(len(inputs)))
-#      print("Lines out " + str(len(output)))
-
-# Version 1 using an array
-#      output = [x for x in inputs if x[2] in req_forms]  # list
-
-# version 2 using a dictionary
-#with open(file_to_write) as csv_file:
-#    csv_reader = csv.reader(csv_file, delimiter=',')
-#    for row in csv_reader:
-#        key = row[0] + "-" + row[1]
-#        if key not in CIK_Name_Unique:
-#            CIK_Name_Unique.update({key : row})
-#            linesOut = linesOut + 1
-# (pd.DataFrame.from_dict(data=CIK_Name_Unique, orient='index')
-#   .to_csv(CIK_Name_to_write, header=False))
-
-# Another version
-#        output = {k : v for k, v in input.items() if v[1] in req_forms}
-#        with open(file_to_write, 'w') as data:
-#            data.write(str(output))
+CIK_Name_Unique = set()
+for row in filtered_Filings:
+    CIK_Name_Unique.add((row[0],row[1]))
+write_records_no_header(file_to_write("Filtered_CIK_Name"), CIK_Name_Unique)
